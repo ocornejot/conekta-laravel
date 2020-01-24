07100b38b4ef220268c21e2f102bd5e2526ef451
Octavio Cornejo <octavio.cornejo@nuvemtecnologia.mx>
2020-01-24 17:04:20 -0600

    Pruebas de publishes.

--- src/ConektaServiceProvider.php
+++ src/ConektaServiceProvider.php
@@ -10,6 +10,17 @@ class ConektaServiceProvider extends ServiceProvider {
     {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/views', 'conekta-laravel');
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/conekta.php',
+            'conekta'
+        );
+        $this->publishes([
+            __DIR__ . '/config/conekta.php' => config_path('conekta.php'),
+            __DIR__ . '/views' => resource_path('views/vendor/conekta'),
+        ]);
+        $this->publishes([
+            __DIR__ . '/Http/Controllers/ConektaController.php' => app_path('Http/Controllers/ConektaController.php'),
+        ]);
     }
 
     public function register()
